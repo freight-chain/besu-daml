21dd69f37278e35109d076a365e91717546f12ee
Restyled.io <commits@restyled.io>
2020-05-20 11:59:09 +0000

    Restyled by shfmt

--- privacy-contracts/src/main/solidity/generateWrappers.sh
+++ privacy-contracts/src/main/solidity/generateWrappers.sh
@@ -9,17 +9,17 @@ OnChainPrivacyGroupManagementProxy
 for target in "$targets"; do
 
   solc --overwrite --bin --abi \
-        -o build  \
-        "$target".sol
+    -o build \
+    "$target".sol
 
 done
 
 for target in "$targets"; do
 
-    web3j solidity generate \
-        -b build/"$target".bin \
-        -a build/"$target".abi \
-        -o ../java \
-        -p org.hyperledger.besu.privacy.contracts.generated
+  web3j solidity generate \
+    -b build/"$target".bin \
+    -a build/"$target".abi \
+    -o ../java \
+    -p org.hyperledger.besu.privacy.contracts.generated
 
 done
